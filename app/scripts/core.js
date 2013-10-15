77dde7a7b9836e0146f429440caf99358253c91d
unknown <Daniel King@.(none)>
2013-10-15 15:15:12 +0800

    base env built.

---
+++ app/scripts/core.js
@@ -0,0 +1,18 @@
+'use strict';
+
+if(typeof angular !== 'undefined') {
+	angular.module('jos.core', []);
+	angular.module('jos.core').controller('menu', ['$scope', function($scope){
+		var win = chrome.app.window.current();
+		$scope.fullscreen = function() {
+			if(win.isFullscreen()) {
+				win.restore();
+			} else {
+				win.fullscreen();
+			}
+		};
+		$scope.quit = function() {
+			win.close();
+		};
+	}]);
+}
---
+++ app/scripts/main.js
@@ -0,0 +1,16 @@
+'use strict';
+/**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/trunk/apps/experimental.app.html
+ * @see http://developer.chrome.com/trunk/apps/app.window.html
+ */
+chrome.app.runtime.onLaunched.addListener(function() {
+    chrome.app.window.create('index.html', {
+    	bounds: {
+	        width: 800,
+	        height: 600
+	    },
+        frame: 'none'
+    });
+});
